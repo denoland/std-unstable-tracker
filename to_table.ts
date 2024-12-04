7827ec7668f0c5119a12bc3ba5a27ca722edd97b
Yoshiya Hinosawa <stibium121@gmail.com>
2024-12-04 16:45:19 +0900

    chore: change table format

--- to_table.ts
+++ to_table.ts
@@ -1,26 +1,32 @@
 import unstableApis from "./unstable_apis.json" with { type: "json" };
 import unstablePackages from "./unstable_packages.json" with { type: "json" };
 import { formatDate } from "./utils.ts";
-
-function unstableApiTable() {
-  console.log(`### Unstable APIs
-
-| Package | Specifier  | Started at |
-| ------- | ---------- | ---------- |`);
-  for (const api of unstableApis) {
-    const date = formatDate(new Date(api.startedAt));
-    console.log(`| ${api.package} | ${api.specifier} | ${date} |`);
-  }
-}
+import { sortBy } from "jsr:@std/collections";
 
 function unstablePackageTable() {
+  const packages = sortBy(unstablePackages, (pkg) => new Date(pkg.startedAt));
   console.log(`### Unstable packages
 
 | Package | Current version | Started at |
 | ------- | --------------- | ---------- |`);
-  for (const pkg of unstablePackages) {
+  for (const pkg of packages) {
     const date = formatDate(new Date(pkg.startedAt));
     console.log(`| ${pkg.name} | ${pkg.version} | ${date} |`);
+  }
+}
+
+function unstableApiTable() {
+  const apis = sortBy(unstableApis, (api) => new Date(api.startedAt));
+  console.log(`### Unstable APIs
+
+| Package | Path  | Started at |
+| ------- | ----- | ---------- |`);
+  for (const api of apis) {
+    const specifier = api.specifier.split("/");
+    const path = specifier.pop();
+    const pkg = specifier.join("/");
+    const date = formatDate(new Date(api.startedAt));
+    console.log(`| ${pkg} | ${path} | ${date} |`);
   }
 }
 
